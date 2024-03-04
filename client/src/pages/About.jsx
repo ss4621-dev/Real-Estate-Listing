59258a3b1ff81d3dc0b9dad54db37e20c4542c22
ss4621-dev <sshekhar1609@gmail.com>
2024-03-04 23:26:28 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Real Estate
+      </h1>
+      <p classname="mb-4 text-slate-700 ">
+        Real estate is a vast and dynamic sector encompassing the buying,
+        selling, and leasing of properties, including land, residential homes,
+        commercial buildings, and industrial spaces.
+      </p>
+
+      <p classname="mb-4 text-slate-700 ">
+        Residential real estate comprises single-family homes, condominiums,
+        townhouses, and multifamily properties, catering to individuals and
+        families seeking shelter and community.
+      </p>
+      <p classname="mb-4 text-slate-700 ">
+        Real estate transactions involve various stakeholders, including buyers,
+        sellers, agents, brokers, lenders, appraisers, and attorneys. The
+        process typically involves property valuation, negotiation, due
+        diligence, financing, and legal documentation.
+      </p>
+    </div>
+  );
 }
